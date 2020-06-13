3bd6d963c6b633031b2130ecd4df7daa6ed17fc0
Fayti1703 <Fayti1703@loop.de>
2020-06-13 10:03:33 +0200

    Add some Xml Extensions

---
+++ Pathfinder/Util/XmlExtensions.cs
@@ -0,0 +1,13 @@
+using System.Xml;
+
+namespace Pathfinder.Util {
+	public static class XmlExtensions {
+		public static string toLogString(this XmlReader rdr) {
+			return rdr.EOF
+				? "Reader reached EOF"
+				: "Reader at " +
+				  (rdr.IsEmptyElement ? "Self-closing" : "") +
+				  $"{rdr.NodeType.ToString()} Node '{rdr.Name}'";
+		}
+	}
+}
