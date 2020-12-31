0b734da7066db395db8d4b3c57d35e2fa24d2c4a
Fayti1703 <Fayti1703@loop.de>
2021-01-01 00:47:03 +0100

    Clean up SaveTrace stack-trace
    Minor change, but the internals of the logging mechanisms should
    no longer be stuck at the top of the stack trace. Eww.

--- Pathfinder/PathfinderHooks.cs
+++ Pathfinder/PathfinderHooks.cs
@@ -236,13 +236,13 @@ namespace Pathfinder {
 
 		[Patch("Hacknet.OS.saveGame")]
 		public static void onDebugHook_saveBeginThread() {
-			DebugLogger.Log(SaveTrace, () => "Save thread triggered \n" + new System.Diagnostics.StackTrace(1));
+			DebugLogger.Log(SaveTrace, () => "Save thread triggered \n" + new System.Diagnostics.StackTrace(3));
 		}
 
 		[Patch("Hacknet.OS.threadedSaveExecute")]
 		public static void onDebugHook_saveFromThread() {
 			DebugLogger.Log(SaveTrace,
-				() => "Threaded save execution triggered \n" + new System.Diagnostics.StackTrace(1)
+				() => "Threaded save execution triggered \n" + new System.Diagnostics.StackTrace(3)
 			);
 		}
 
